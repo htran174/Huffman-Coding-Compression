8659e3de26a54af4b6548036595df7507c5f33e1
Hien Tran <htran174@csu.fullerton.edu>
2025-03-18 22:11:18 -0700

    added header and delted main

--- backend.py
+++ backend.py
@@ -1,6 +1,15 @@
+"""
+======================================================================================================
+
+                                            backend.py
+                                        Created by Hien Tran
+                                Holds huffman function to build tree and nodes
+
+======================================================================================================
+"""
+
 import heapq
 from collections import Counter
-import graph
 
 #Node class
 class Node:
@@ -56,37 +65,3 @@ def decode(encoded_text, codes):
     return decoded_text
 
 
-if __name__ == "__main__":
-    
-    # hardcoded text example
-    text = "Huffman coding is a lossless data compression algorithm. It is used in various applications including file compression formats like ZIP. The algorithm follows a greedy approach by assigning variable-length codes to characters, ensuring optimal encoding efficiency."
-    print(f"\nOriginal text: {text[:50]}... (truncated for display)")
-
-    # Build Huffman Tree
-    huffman_root = build_huffman_tree(text)
-
-    # Generate Huffman Codes
-    huffman_codes = create_codes(huffman_root)
-    print("\nHuffman Codes Generated:")
-    for char, code in sorted(huffman_codes.items(), key=lambda x: len(x[1])):  # Sorted by code length
-        print(f"{repr(char)}: {code}")  # Use repr() to display special characters properly
-
-    # Encode input text
-    encoded_str = encode(text, huffman_codes)
-    print(f"\nEncoded (first 100 bits): {encoded_str[:100]}... (truncated for display)")
-
-    # Decode back
-    decoded_text = decode(encoded_str, huffman_codes)
-    print(f"\nDecoded text matches: {text == decoded_text}")
-
-    if text == decoded_text:
-        print("\nDecoding successful!")
-    else:
-        print("\nDecoding failed! Mismatch detected.")
-
-    # Draw Huffman Tree and Huffman Code Table
-    tree_graph = graph.draw_huffman_tree(huffman_root)
-    code_graph = graph.draw_huffman_codes_table(huffman_codes)
-
-    tree_graph.render("huffman_tree", view=True)  # Save & open Huffman tree
-    code_graph.render("huffman_codes", view=True)  # Save & open Huffman codes
